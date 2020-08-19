4f0e7678e8cc70b88f53452770b20ad61adcab56
iamsunhao <iamsunhao@protonmail.com>
2020-08-19 22:57:33 +0800

    update main.go

--- main.go
+++ main.go
@@ -8,9 +8,20 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Args is too short!")
+	if len(os.Args) == 1 {
+		fmt.Println(`操作说明：
+将通过管道传入的标准输入进行替换。最多接受两个参数，多余的参数将被丢弃。
+		
+repl [正则表达式]
+	将通过正则表达式匹配到的内容替换为空。
+		
+repl [正则表达式] substring
+	将通过正则表达式匹配到的内容替换为substring。`)
 		os.Exit(0)
+	}
+	substring := ""
+	if len(os.Args) > 2 {
+		substring = os.Args[2]
 	}
 	re := regexp.MustCompile(os.Args[1])
 	var raw string
@@ -21,6 +32,6 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "Reading standard input:", err)
 	}
-	output := re.ReplaceAllLiteralString(raw, os.Args[2])
+	output := re.ReplaceAllLiteralString(raw, substring)
 	fmt.Println(output)
 }
