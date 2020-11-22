7ee99e716dee6773f5456a6a09e2cd4bf9ce6a1d
Sun Hao <89498522@qq.com>
2020-11-22 18:15:48 +0800

    update

--- main.go
+++ main.go
@@ -10,32 +10,32 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Println(
-			`操作说明：
-将通过管道传入的标准输入进行替换。最多接受两个参数，多余的参数将被丢弃。
-		
-repl [正则表达式] [substring]
-	将通过正则表达式匹配到的内容替换为空。
-		
-repl [正则表达式] [substring]
-	将通过正则表达式匹配到的内容替换为[substring]。
-
-例：
-	echo "abc123" | repl "[a-z]" "0"
-	输出：000123`)
+		fmt.Printf("说明：%s regexp string\n", os.Args[0])
+		fmt.Printf("示例一：\n")
+		fmt.Printf("  标准输入：Hello,123\n")
+		fmt.Printf("  %s [0-9] A\n", os.Args[0])
+		fmt.Printf("  标准输出：Hello,AAA\n")
+		fmt.Printf("示例二：\n")
+		fmt.Printf("  标准输入：Hello,123\n")
+		fmt.Printf("  %s [0-9]\n", os.Args[0])
+		fmt.Printf("  标准输出：Hello,\n")
 		os.Exit(0)
 	}
 	substring := ""
 	if len(os.Args) > 2 {
 		substring = os.Args[2]
 	}
-	re := regexp.MustCompile(os.Args[1])
+	re, err := regexp.Compile(os.Args[1])
+	if err != nil {
+		fmt.Printf("%s 是一个非法的正则表达式。", os.Args[1])
+		os.Exit(1)
+	}
 
 	output := re.ReplaceAllLiteralString(GetStdio(), substring)
 	fmt.Println(output)
 }
 
-// 将标准输入原样转为字符串（总会多一行空行）
+// 将标准输入原样转为字符串
 func GetStdio() string {
 	var sb strings.Builder
 	scanner := bufio.NewScanner(os.Stdin)
@@ -43,8 +43,9 @@ func GetStdio() string {
 		sb.WriteString(scanner.Text() + "\n")
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Reading standard input:", err)
+		fmt.Fprintln(os.Stderr, "读取标准输入出错：", err)
 	}
+	// 去掉从标准输入读取字符串时多出的一个空行
 	// 换行符\n在字符串中只占一个长度
 	return sb.String()[0:(sb.Len() - 1)]
 }
